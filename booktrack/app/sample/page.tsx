813e6a22ece16b54e472a962c8d00fa109ca2fdd
JohnMalvin <johnmalvin068@gmail.com>
2025-12-29 22:55:39 +0700

    feat: comment out unused API fetch logic in Home component

--- booktrack/app/sample/page.tsx
+++ booktrack/app/sample/page.tsx
@@ -1,42 +1,44 @@
-export default async function Home() {
-  const res = await fetch(
-    `/api/testAPI`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      }
-    } 
-  );
-  if (!res.ok) {
-    const text = await res.text();
-    console.error("POST failed:", text);
-    throw new Error("POST failed");
-  }
-  const data = await res.json();
+// "use client";
 
-  const res2 = await fetch(
-    `/api/testAPI`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: "Dean"
-      })
-  }
-  );
+// export default async function Home() {
+//   const res = await fetch(
+//     `/api/testAPI`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       }
+//     } 
+//   );
+//   if (!res.ok) {
+//     const text = await res.text();
+//     console.error("POST failed:", text);
+//     throw new Error("POST failed");
+//   }
+//   const data = await res.json();
 
-  if (!res2.ok) {
-    const text = await res2.text();
-    console.error("POST failed:", text);
-    throw new Error("POST failed");
-  }
-  const data2 = await res2.json();
+//   const res2 = await fetch(
+//     `/api/testAPI`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         name: "Dean"
+//       })
+//   }
+//   );
 
-  return (
-    <>
-      <p>{data.message}</p>
-      <p>{data2.message}</p>
-    </>
-  )
-}
+//   if (!res2.ok) {
+//     const text = await res2.text();
+//     console.error("POST failed:", text);
+//     throw new Error("POST failed");
+//   }
+//   const data2 = await res2.json();
+
+//   return (
+//     <>
+//       <p>{data.message}</p>
+//       <p>{data2.message}</p>
+//     </>
+//   )
+// }
