4cae7619ee3ec7f050d066a920b80ef67add486c
JohnMalvin <johnmalvin068@gmail.com>
2025-12-29 22:58:19 +0700

    feat: enable client-side rendering in SamplePage component

--- booktrack/app/sample/page.tsx
+++ booktrack/app/sample/page.tsx
@@ -1,5 +1,7 @@
-// "use client";
-
+"use client";
+export default function SamplePage() {
+	return <div>Hello</div>;
+}
 // export default async function Home() {
 //   const res = await fetch(
 //     `/api/testAPI`, {
