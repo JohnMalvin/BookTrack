139d5aa8482201337e93766df24ca58fbf6565b3
JohnMalvin <johnmalvin068@gmail.com>
2025-12-29 22:47:17 +0700

    feat: remove unused createUser function call from room creation API

--- booktrack/app/api/createRoom/route.ts
+++ booktrack/app/api/createRoom/route.ts
@@ -1,11 +1,9 @@
 export const runtime = "nodejs";
 
-import { createUser } from '@/lib/UserFunctions';
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
 	const { name } = await request.json();
-	await createUser();
 	return NextResponse.json({ message: `Hello ${name}` });
 }
 
